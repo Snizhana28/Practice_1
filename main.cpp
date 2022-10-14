400cd44ee9ecbb2c8c90d8be44c9dc17764f7a61
snizhana28 <snizhana2812kostyuk@gmail.com>
2022-10-14 17:48:01 +0300

    Done practic 1.4

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
+/*Task 4. The user enters numbers from the keyboard. Calculate their sum and display it on the screen as soon as the user
+will enter zero.*/
 #include <iostream>
 using namespace std;
 
 int main()
 {
-
+	int i;
+	int suma = 0;
+	for (;;) 
+	{ 
+		cout << "Enter a number : ";
+		cin >> i;
+	
+		if (i == 0)
+			break;
+		else
+			suma += i;
+	}
+	cout << "Suma : " << suma << endl;
 	return 0;
 }
