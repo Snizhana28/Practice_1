4722fff4e99bf0ad7d6519b2dea6ce42ce6d709f
snizhana28 <snizhana2812kostyuk@gmail.com>
2022-10-14 17:39:33 +0300

    Done practic 1.1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
+/*Task 1. Display on the screen all numbers from zero to the number entered by the user.*/
 #include <iostream>
 using namespace std;
 
 int main()
 {
-
+	int x;
+	cout << "Enter a number : ";
+	cin >> x;
+	for (int i = 0; i <= x; i++) 
+	{
+		cout << i << "\t";
+	}
 	return 0;
 }
