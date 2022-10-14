1c1867020dfb607aff9171e7a5ce71863ea34ceb
snizhana28 <snizhana2812kostyuk@gmail.com>
2022-10-14 17:45:23 +0300

    Done practice 1.3

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
+/*The user enters two range limits. Calculate the sum of all numbers in a range*/
 #include <iostream>
 using namespace std;
 
 int main()
 {
-
+	int x, y;
+	  cout << "Enter 1 number of the range: ";
+	  cin >> x;
+	  cout << "Enter 2 number of the range: ";
+	  cin >> y;
+	int suma = 0;
+	int i = x;
+	while (i <= y)
+	{
+		suma += i;
+		i++;
+	}
+	cout << "Suma : " << suma << endl;
 	return 0;
 }
